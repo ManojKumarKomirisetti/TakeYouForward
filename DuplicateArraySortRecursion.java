8413c75ad135d441e525652d14b6d6879129aa64
ManojKumarKomirisetti <manojkumarkomirisetti@gmail.com>
2024-09-07 23:15:50 +0530

    TUF Array Duplicates Recursion 7/9/24

---
+++ DuplicateArraySortRecursion.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class DuplicateArraySortRecursion
+{
+    static int A[] = {1,2,5,2,4,3,9,4,5,3,2,53,52,2,6,2,5,1,8,4,6,9,2252,4,1,2,5,52,2252};
+
+    public static void remove()
+    {
+        if()
+        {
+        for(int i = 0;i<A.length;i++)
+        {
+            System.out.println(A[i]);
+        }
+    }
+    } 
+    public static void main(String args[])
+    {
+        Arrays.sort(A);
+        remove();
+
+    }
+}
